7316e504bea22274ad416f9208f1636a46bdd612
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-01 16:28:02 +0100

    Create NonParallelNonManifoldSystemsSDKOne.h

---
+++ NonParallelNonManifoldSystemsSDKOne.h
@@ -0,0 +1,4 @@
+//the non parallel behaviour of a system tuning at 5250 V or less can be ploted
+//as a sub holomorph space as a vizualised non ploted previous state. This pattern seems the only one in a SDK.
+//to enforce SDK for cpu, we need to commit many psh commands if the voltage increase. So we canot loss datas.
+//The best thing would not be to normalize datas but signals that can be rendered in laptop or pc screens !
