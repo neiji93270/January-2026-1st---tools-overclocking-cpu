245b298f1213994dfda377ed8a28d261ba08cbe8
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-01 16:30:51 +0100

    Create FullyHolomorphFunctions.h

---
+++ FullyHolomorphFunctions.h
@@ -0,0 +1,3 @@
+//The holomorph functions seems not holomorph equations if the cpu architecture reject at least one signal.
+//so holomorph equation that check the cpu pins are in the secod step of the main scheduller algorithm.
+//This scheduller is also itself an SDK or a reactive toolchain.
